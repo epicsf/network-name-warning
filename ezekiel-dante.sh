8bea2b5ae86fc673ae6e7168d9ee95793754be07
Todd Eichel <todd@toddeichel.com>
2018-01-14 16:52:03 -0800

    Add note about restarting DVS after name change

--- ezekiel-dante.sh
+++ ezekiel-dante.sh
@@ -3,4 +3,4 @@
 # for safety: https://sipb.mit.edu/doc/safe-shell/
 set -uf -o pipefail
 
-./network-name-warning.sh "Ezekiel" "This likely breaks Graphics PC audio routing to the audio console.\n\nYou should set the name back to Ezekiel in the Sharing tab of System Preferences."
+./network-name-warning.sh "Ezekiel" "This likely breaks Graphics PC audio routing to the audio console.\n\nYou should set the name back to Ezekiel in the Sharing tab of System Preferences, then restart Dante Virtual Soundcard."
